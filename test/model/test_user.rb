758634491565048ffd1f03ffd6824ebb9e4277fc
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-03-26 16:15:29 +0200

    Skeleton for User model test

---
+++ test/model/test_user.rb
@@ -0,0 +1,7 @@
+require File.dirname(__FILE__) + '/../../fraenk.rb'
+require File.dirname(__FILE__) + '/../helper.rb'
+
+class TestUserController < Test::Unit::TestCase
+  include Rack::Test::Methods
+end
+
