0635aa35252360eb229d6afdcd5484a3139653c0
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2010-03-26 14:01:38 +0200

    Before filter to set title/subtitle

--- lib/app.rb
+++ lib/app.rb
@@ -2,7 +2,12 @@
 class TestApp < Sinatra::Base
   set :sessions, true
   set :public, File.dirname(__FILE__) + '/../public'
-  
+ 
+  before do
+    @title = "Sinatra + Haml + Bundler test"
+    @subtitle ="Just a bit of something I got up to one day"
+  end
+
   get '/' do
     haml :index
   end
