fcb2af7b096cbcb9e8752b31cb9f34f86a4255fd
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2010-03-26 17:01:59 +0200

    User model and a test

--- test/model/test_user.rb
+++ test/model/test_user.rb
@@ -1,7 +1,12 @@
-require File.dirname(__FILE__) + '/../../fraenk.rb'
+require File.dirname(__FILE__) + '/../../bootstrap.rb'
+require File.dirname(__FILE__) + '/../../lib/model/user.rb'
 require File.dirname(__FILE__) + '/../helper.rb'
 
 class TestUserController < Test::Unit::TestCase
   include Rack::Test::Methods
+
+  context "A User instance" do
+    @user = User.new
+  end
 end
 
