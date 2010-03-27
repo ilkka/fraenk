1e57d03d5577c4752f1721606935903fd49a9b84
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-03-27 14:54:53 +0200

    Setup datamapper

--- bootstrap.rb
+++ bootstrap.rb
@@ -9,3 +9,14 @@ end
 
 Bundler.require
 
+# load db config
+Environment = ENV['SINATRA_ENV'] || 'development'
+DBConfig = YAML::load(File.open('db/config.yml'))[Environment]
+
+# configure datamapper
+DataMapper::Logger.new($stdout, :debug)
+case DBConfig['adapter']
+when 'sqlite3'
+  DataMapper.setup(:default, "sqlite3:#{DBConfig['database']}")
+end
+
