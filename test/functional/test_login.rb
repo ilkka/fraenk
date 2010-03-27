8674cb777f8d215d74375bd85cb24fa0dc21d8b2
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-03-27 16:15:48 +0200

    Test for login handler

---
+++ test/functional/test_login.rb
@@ -0,0 +1,35 @@
+require File.dirname(__FILE__) + '/../../fraenk.rb'
+require File.dirname(__FILE__) + '/../helper.rb'
+
+class TestLoginHandler < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    FraenkApp
+  end
+
+  context "The login handler" do
+    should "let user login" do
+    end
+
+    should "let user logout" do
+    end
+
+    should "let user register for an account" do
+    end
+
+    should "handle activation via GET" do
+    end
+
+    should "handle activation via POST" do
+    end
+
+    should "enable user to resend activation" do
+    end
+
+    should "enable user to reset forgotten password" do
+    end
+
+  end
+end
+
