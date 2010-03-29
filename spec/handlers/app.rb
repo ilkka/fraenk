053cd57c26da8decd93f5b1aedf30b7fefaf1d6e
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-03-29 21:01:36 +0300

    First test for the default handler

---
+++ spec/handlers/app.rb
@@ -0,0 +1,13 @@
+require File.join(File.dirname(__FILE__) + '..' + 'spec_helper.rb')
+
+describe 'The default handler' do
+  def app
+    @app ||= FraenkApp
+  end
+
+  should "return a valid index page" do
+    get '/'
+    last_response.should be_ok
+  end
+end
+
