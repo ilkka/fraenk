a0843e941563b879bd01b08f86e04d2bfd69cecc
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-04-01 20:17:08 +0300

    Removed authlogic require

--- fraenk.rb
+++ fraenk.rb
@@ -5,6 +5,11 @@
 # bootstrap bundler
 require 'bootstrap.rb'
 
+# load models and handlers
+Dir.glob(File.join(File.dirname(__FILE__), 'lib', '*', '*.rb')).each do |file|
+  require file
+end
+
 # load my app lib
 require 'lib/app.rb'
 
